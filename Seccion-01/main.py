fc82a06d400c7f984bc31813191c2a9b7512a05a
Menthealth <info.luisdev@gmail.com>
2024-04-27 09:18:53 -0300

    First Commit

---
+++ Seccion-01/main.py
@@ -0,0 +1,14 @@
+num1 = float(input("Ingrese el primer número -> "))
+num2 = float(input("Ingrese el segundo número -> "))
+
+cuadrado_producto = (num1 * num2) ** 2
+print(f"El cuadrado del producto es -> {cuadrado_producto}")
+
+doble_cociente = 2 * (num1 / num2)
+print(f"El doble del cociente es -> {doble_cociente}")
+
+cuadruple = 4 * (num1 - num2)
+print(f"El cuádruple de la diferencia es -> {cuadruple}")
+
+sum_cuadrado = (num1 + num2) ** 2
+print(f"El cuadrado de la suma es -> {sum_cuadrado}")
