92255a39a557dab5920278f9a20f3f72dba1eeac
Menthealth <info.luisdev@gmail.com>
2024-06-22 09:00:03 -0400

    añadiendo clases reservas y cliente

--- Proyecto_final/main (1).py
+++ Proyecto_final/main (1).py
@@ -4,3 +4,19 @@ class Cliente:
     def __init__(self, nombre):
         self.nombre = nombre
 
+class Reserva:
+    def __init__(self, id, cliente, numero_personas, fecha, mesa):
+        self.id = id
+        self.cliente = cliente
+        self.numero_personas = numero_personas
+        self.fecha = fecha
+        self.mesa = mesa
+
+    def listar_reserva(self):
+        return {
+            "id": self.id,
+            "nombre_cliente": self.cliente.nombre,
+            "numero_personas": self.numero_personas,
+            "fecha": self.fecha,
+            "mesa": self.mesa
+        }
